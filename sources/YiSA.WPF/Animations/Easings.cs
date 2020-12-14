05fabfe6a9a89c0021f4ab927377974768ee6707
yuki.tsuneyama <at12k313@gmail.com>
2020-12-15 05:52:00 +0900

    easing

--- sources/YiSA.WPF/Animations/Easings.cs
+++ sources/YiSA.WPF/Animations/Easings.cs
@@ -4,6 +4,19 @@ using System.Windows.Media.Animation;
 
 namespace YiSA.WPF.Animations
 {
+    public enum EaseType
+    {
+        Cubic,
+        EaseIn,
+        EaseOut,
+        EaseInElastic,
+        EaseOutElastic,
+        EaseInOutElastic,
+        EaseInBack,
+        EaseOutBack,
+        EaseInOutBack,
+    }
+    
     public class CubicEasing : IEasingFunction
     {
         public double Ease(double t)
@@ -27,10 +40,99 @@ namespace YiSA.WPF.Animations
             return t * ( 2 - t );
         }
     }
+    public class EaseInElasticEasing : IEasingFunction
+    {
+        public double Ease(double t)
+        {
+            const double c4 = (2 * Math.PI) / 3;
+
+            if (t is 0)
+                return 0;
+
+            if (Math.Abs(t - 1) < 0.0001d)
+                return 1;
+            
+            return -Math.Pow(2, 10 * t - 10) * Math.Sin((t * 10 - 10.75) * c4);
+        }
+    }
+
+    public class EaseOutElasticEasing : IEasingFunction
+    {
+        public double Ease(double t)
+        {
+            const double c4 = (2 * Math.PI) / 3;
+
+            if (t is 0)
+                return 0;
+
+            if (Math.Abs(t - 1) < 0.0001d)
+                return 1;
+            
+            return Math.Pow(2, -10 * t) * Math.Sin((t * 10 - 0.75) * c4) + 1;
+        }
+    }
+
+    public class EaseInOutElasticEasing : IEasingFunction
+    {
+        public double Ease(double t)
+        {
+            const double c5 = (2 * Math.PI) / 4.5;
+
+            if (t is 0)
+                return 0;
+
+            if (Math.Abs(t - 1) < 0.0001d)
+                return 1;
+
+            if (t < 0.5)
+            {
+                return -(Math.Pow(2, 20 * t - 10) * Math.Sin((20 * t - 11.125) * c5)) / 2;
+            }
+            
+            return (Math.Pow(2, -20 * t + 10) * Math.Sin((20 * t - 11.125) * c5)) / 2 + 1;
+        }
+    }
+
+    public class EaseInBackEasing : IEasingFunction
+    {
+        public double Ease(double t)
+        {
+            const double c1 = 1.70158;
+            const double c3 = c1 + 1;
+
+            return c3 * t * t * t - c1 * t * t;
+        }
+    }
+
+    public class EaseOutBackEasing : IEasingFunction
+    {
+        public double Ease(double t)
+        {
+            const double c1 = 1.70158;
+            const double c3 = c1 + 1;
+
+            return 1 + c3 * Math.Pow(t - 1, 3) + c1 * Math.Pow(t - 1, 2);
+        }
+    }
+
+    public class EaseInOutBackEasing : IEasingFunction
+    {
+        public double Ease(double t)
+        {
+            const double c1 = 1.70158;
+            const double c2 = c1 * 1.525;
+
+            if (t < 0.5)
+            {
+                return (Math.Pow(2 * t, 2) * ((c2 + 1) * 2 * t - c2)) / 2;
+            }
+            
+            return (Math.Pow(2 * t - 2, 2) * ((c2 + 1) * (t * 2 - 2) + c2) + 2) / 2;
+        }
+    }
 
     public class SnapEasing : IEasingFunction
     {
-        private readonly int _step;
         private readonly double[] _table;
         private readonly IEasingFunction? _easingFunction;
 
@@ -39,7 +141,6 @@ namespace YiSA.WPF.Animations
             if(step <= 0)
                 throw new ArgumentException(nameof(step));
 
-            _step = step;
             _easingFunction = easingFunction;
             _table = new double[step + 1];
             _table[0] = 0d;
--- sources/YiSA.WPF/MarkupEx/EasingExtension.cs
+++ sources/YiSA.WPF/MarkupEx/EasingExtension.cs
@@ -19,12 +19,29 @@ namespace YiSA.WPF.MarkupEx
         }
     }
     
-    public class CubicEasingExtension : MarkupExtension
+    public class EasingExtension : MarkupExtension
     {
-        private readonly IEasingFunction _easing = new CubicEasing();
+        private EaseType _easeType;
+        public EasingExtension(EaseType easeType)
+        {
+            
+        }
+        
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            return _easing;
+            return _easeType switch 
+            {
+                EaseType.Cubic => new CubicEasing(),
+                EaseType.EaseIn => new EaseInEasing(),
+                EaseType.EaseOut => new EaseOutEasing(),
+                EaseType.EaseInBack => new EaseInBackEasing(),
+                EaseType.EaseOutBack => new EaseOutBackEasing(),
+                EaseType.EaseInOutBack => new EaseInOutBackEasing(),
+                EaseType.EaseInElastic=> new EaseInElasticEasing(),
+                EaseType.EaseOutElastic => new EaseOutElasticEasing(),
+                EaseType.EaseInOutElastic => new EaseInOutElasticEasing(),
+                _ => new CubicEasing()
+            };
         }
     }
     
