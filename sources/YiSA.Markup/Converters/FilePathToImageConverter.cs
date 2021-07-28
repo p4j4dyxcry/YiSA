afe302674c0003089b5ed64608a2f772dbf449c8
yuki.tsuneyama <at12k313@gmail.com>
2021-07-28 15:38:42 +0900

    ファイルパスから画像へ変換するコンバーター追加

---
+++ sources/YiSA.Markup/Converters/FilePathToImageConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace YiSA.Markup.Converters
+{
+    public class FilePathToImageConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string filePath)
+            {
+                try
+                {
+                    BitmapImage image = new BitmapImage();
+                    image.BeginInit();
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.UriSource = new Uri(filePath);
+                    image.EndInit();
+                    return image;
+                }
+                catch
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ sources/YiSA.Markup/Extension/FileToImageExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Markup;
+using YiSA.Markup.Converters;
+
+namespace YiSA.Markup.Extension
+{
+    public class FileToImageExtension: MarkupExtension
+    {
+        private static FilePathToImageConverter? _converter;
+        public override object ProvideValue( IServiceProvider serviceProvider )
+        {
+            return _converter ??= new FilePathToImageConverter();
+        }
+    }
+}
