66b07b0680acd58fac5b77ce227944fa249e0938
p4j4dyxcyr <11988607+p4j4dyxcry@users.noreply.github.com>
2021-08-17 07:00:39 +0900

    update c# version

--- sources/YiSA.Foundation/CommandLine/Parser.cs
+++ sources/YiSA.Foundation/CommandLine/Parser.cs
@@ -255,56 +255,25 @@ namespace YiSA.Foundation.CommandLine
         {
             try
             {
-                switch (value)
+                value = value switch
                 {
-                    case int _:
-                        value = int.Parse(arg);
-                        break;
-                    case short _:
-                        value = short.Parse(arg);
-                        break;
-                    case long _:
-                        value = long.Parse(arg);
-                        break;
-                    case uint _:
-                        value = uint.Parse(arg);
-                        break;
-                    case ushort _:
-                        value = ushort.Parse(arg);
-                        break;
-                    case ulong _:
-                        value = ulong.Parse(arg);
-                        break;
-                    case float _:
-                        value = float.Parse(arg);
-                        break;
-                    case double _:
-                        value = double.Parse(arg);
-                        break;
-                    case decimal _:
-                        value = decimal.Parse(arg);
-                        break;
-                    case char _:
-                        value = char.Parse(arg);
-                        break;
-                    case string _:
-                        value = arg;
-                        break;
-                    case byte _:
-                        value = byte.Parse(arg);
-                        break;
-                    case sbyte _:
-                        value = sbyte.Parse(arg);
-                        break;
-                    case bool _:
-                        value = bool.Parse(arg);
-                        break;
-                    case Enum _:
-                        value = Enum.Parse(value.GetType(), arg);
-                        break;
-                    default:
-                        return;
-                }
+                    int _    => int.Parse(arg),
+                    short _  => short.Parse(arg),
+                    long _   => long.Parse(arg),
+                    uint _   => uint.Parse(arg),
+                    ushort _ => ushort.Parse(arg),
+                    ulong _  => ulong.Parse(arg),
+                    float _  => float.Parse(arg),
+                    double _ => double.Parse(arg),
+                    decimal _=> decimal.Parse(arg),
+                    char _   => char.Parse(arg),
+                    string _ => arg,
+                    byte _   => byte.Parse(arg),
+                    sbyte _  => sbyte.Parse(arg),
+                    bool _   => bool.Parse(arg),
+                    Enum _   => Enum.Parse(value.GetType(), arg),
+                    _ => value
+                };
             }
             catch (Exception)
             {
